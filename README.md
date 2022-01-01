f3fa7ffebcb24c717d46a1a639b3e4a4e120bcff
clabbott <clark.a.abbott@gmail.com>
2022-01-01 12:06:40 -0500

    Iteration 0

---
+++ README.md
@@ -0,0 +1,2 @@
+# iteration 0
+# goal: create looping structure that runs NUM times and prints status each time 
---
+++ script.py
@@ -0,0 +1,12 @@
+# Global parameters 
+CYCLE_COUNT = 0
+MAX_CYCLES = 5
+
+
+
+
+# BEGIN CYCLE 
+while CYCLE_COUNT!=MAX_CYCLES:
+    print("BOC" , CYCLE_COUNT)
+    print("EOC" , CYCLE_COUNT)
+    CYCLE_COUNT+=1
